721a8a953b5fc5f87fa703c492a46ff45528ad12
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-06-11 00:43:46 -0600

    Update app20.py

--- app20.py
+++ app20.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 
@@ -5,6 +6,13 @@ st.title("Group-020")
 
 st.markdown("""
 This project integrates contributions from multiple groups to analyze and visualize diverse datasets related to air quality, environmental monitoring, and gun violence trends in New Mexico and surrounding states. Each page represents a unique dataset and individual analysis:
+
+---
+
+### **Gun Violence Data Visualization in NM**  
+Contributor: **DT**  
+Using data from Group 12 focused on **gun violence in New Mexico**, this dashboard allows users to view statistics by **date** or **city**, choosing metrics such as *victims injured*, *suspects injured*, *killed*, or *arrested*. All visualizations are presented as **bar charts** for intuitive comparisons.  
+[Streamlit Link](https://untitled2py-2zn5bldhgddpnjthbjxykd.streamlit.app/)
 
 ---
 
@@ -20,10 +28,4 @@ Contributor: **GP**
 Built using air quality data from Group 8, this interactive dashboard enables users to explore AQI factors across five states: **MN**, **WI**, **MI**, **MT**, and **ND**. Users can select counties and view trends using either a **Bar Chart** or **Line Chart** to observe regional environmental differences.  
 [Streamlit Link](https://finalcodegppy-vumfdtkohha8hzlipqdypx.streamlit.app/)
 
----
-
-### **Gun Violence Data Visualization in NM**  
-Contributor: **DT**  
-Using data from Group 12 focused on **gun violence in New Mexico**, this dashboard allows users to view statistics by **date** or **city**, choosing metrics such as *victims injured*, *suspects injured*, *killed*, or *arrested*. All visualizations are presented as **bar charts** for intuitive comparisons.  
-[Streamlit Link](https://untitled2py-2zn5bldhgddpnjthbjxykd.streamlit.app/) """)
-
+ """)
